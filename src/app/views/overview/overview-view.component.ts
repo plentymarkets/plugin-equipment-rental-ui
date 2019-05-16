b4061f8d2a3a449d700bb3b58ad6e562e21b52c0
Kevin Fink <kevin.fink@plentymarkets.com>
2019-05-16 11:08:47 +0200

    rewritten code added breadcrumbs and give article back function

--- src/app/views/overview/overview-view.component.ts
+++ src/app/views/overview/overview-view.component.ts
@@ -1,14 +1,13 @@
-import { 
+import {
     Component,
-    OnInit,
-    ViewChild
+    OnInit
 } from '@angular/core';
 import { ArticleInterface } from '../../core/article.interface';
 import { ExampleDataService } from './overview-view.service';
-import { TerraAlertComponent, 
+import { TerraAlertComponent,
     TerraJsonToFormFieldService,
     TerraOverlayComponent
-} 
+}
 from '@plentymarkets/terra-components';
 import { isArray } from 'util';
 import { TranslationService } from 'angular-l10n';
@@ -20,35 +19,38 @@ import { TranslationService } from 'angular-l10n';
 })
 export class OverviewViewComponent implements OnInit
 {
+
+    public articlesResult:Array<ArticleInterface>; // Array with all results
+    public articles:Array<ArticleInterface>; // Copy of array to filter items
+
+    // Form
+    public firstName:string = '';
+    public lastName:string = '';
+    public email:string = '';
+
+    // Layout
+    private breadcrumbs:Array<string>;
     private leftColumnWidth:number = 0;
     private centerColumnWidth:number = 0;
     private rightColumnWidth:number = 0;
 
     private _actualArticleId:number = 0;
     private _actualArticleKey:any = 0;
-    private _name:string = "";
+    private _name:string = '';
     private _selectedListBoxValue:number = 1;
 
-    public articlesResult:Array<ArticleInterface>; //Array with all results
-    public articles:Array<ArticleInterface>; //Copy of array to filter items
- 
     private _alert:TerraAlertComponent;
-
-    //Form
-    firstName:string = '';
-    lastName:string = '';
-    email:string = '';
 
     constructor(
                 public translation:TranslationService,
                 private _statsDataService:ExampleDataService)
     {
         this._alert = TerraAlertComponent.getInstance();
-    }  
+    }
 
     public ngOnInit():void
     {
-        this.showFirstPage();
+        this.loadPage(1);
 
         this._alert.closeAlertByIdentifier('info');
         this.createArticleData();
@@ -58,39 +60,31 @@ export class OverviewViewComponent implements OnInit
                 type:'info',
                 dismissOnTimeout:3000,
                 identifier: 'info'
-            });   
+            });
     }
 
-    emailAutocomplete(val) {
-        if(this.email.indexOf("@plentymarkets.com") != -1 && (this.firstName.length == 0 || this.lastName.length == 0))
-            this.email = "";
-        if(this.firstName.length == 0 || this.lastName.length == 0)
-            return; 
-        this.email = this.firstName+"."+this.lastName+"@plentymarkets.com";
-    } 
+    public emailAutocomplete(val:string):void {
+        if(this.email.indexOf('@plentymarkets.com') !== -1 && (this.firstName.length === 0 || this.lastName.length === 0))
+        {
+            this.email = '';
+        }
+        if(this.firstName.length == 0 || this.lastName.length == 0) {
+            return;
+        }
+        this.email = this.firstName + '.' + this.lastName + '@plentymarkets.com';
+    }
 
     private rentArticle():void
     {
-        alert("pressed");
+        alert('pressed');
     }
 
-    private showFirstPage():void
+    private loadPage(page:number):void
     {
-        this.leftColumnWidth = 2;
-        this.centerColumnWidth = 10;
-        this.rightColumnWidth = 0;
-    }
-
-    private showSecondPage():void
-    {
-        this.leftColumnWidth = 0;
-        this.centerColumnWidth = 0;
-        this.rightColumnWidth = 12;
-    }
-
-    private openRentOverlay():void
-    {
-        this.rentOverlay.showOverlay();
+        this.leftColumnWidth = (page === 1) ? 2 : 0;
+        this.centerColumnWidth = (page === 1) ? 10 : 0;
+        this.rightColumnWidth = (page === 1) ? 0 : 12;
+        this.breadcrumbs = (page === 1) ? ['Seite 1'] : ['Seite 1', 'Verleihen'];
     }
 
     private onSearchBtnClicked():void
@@ -100,7 +94,7 @@ export class OverviewViewComponent implements OnInit
 
     private onResetBtnClicked():void
     {
-        this._name = "";
+        this._name = '';
         this.updateData();
     }
 
@@ -108,8 +102,8 @@ export class OverviewViewComponent implements OnInit
     {
         this._actualArticleId = id;
         this.setActualArticleKeyById(id);
-        this.showSecondPage();
-    } 
+        this.loadPage(2);
+    }
 
     private setActualArticleKeyById(id:number):void
     {
@@ -124,34 +118,35 @@ export class OverviewViewComponent implements OnInit
 
     private onSubmit():void
     {
-        this.onSearchBtnClicked();       
+        this.onSearchBtnClicked();
     }
 
- 
+
     public updateData():void
     {
         if(this._name.length > 0){
             this.articles = this.articlesResult.filter(
                 article => article.name.toLowerCase().indexOf(this._name.toLowerCase()) > -1
-            );  
+            );
         }
-        else
-            this.articles = this.articlesResult;   
+        else {
+            this.articles = this.articlesResult;
+        }
     }
- 
+
     private createArticleData():void
     {
         this.articlesResult = [];
-        this._statsDataService.getRestCallData("/rest/items?with=itemImages").subscribe((response:Array<any>) =>
+        this._statsDataService.getRestCallData('/rest/items?with=itemImages').subscribe((response:Array<any>) =>
         {
-            if(isArray(response["entries"])){ 
-                for(let article of response["entries"])
+            if(isArray(response['entries'])){
+                for(let article of response['entries'])
                 {
                     this.articlesResult.push(
                         {
                             name: article.texts[0].name1,
                             id: article.id,
-                            image: (article.itemImages.length > 0) ? article.itemImages[0].url : "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/600px-No_image_available.svg.png",
+                            image: (article.itemImages.length > 0) ? article.itemImages[0].url : 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/600px-No_image_available.svg.png',
                             description: article.texts[0].shortDescription,
                             longDescription: article.texts[0].description,
                             created_at: article.created_at
@@ -167,6 +162,6 @@ export class OverviewViewComponent implements OnInit
             });
         }
         );
-        this.articles = this.articlesResult;             
+        this.articles = this.articlesResult;
     }
 }
--- src/app/views/overview/overview-view.service.ts
+++ src/app/views/overview/overview-view.service.ts
@@ -5,8 +5,8 @@ import {
     TerraBaseService,
     TerraLoadingSpinnerService
 } from '@plentymarkets/terra-components';
- 
- 
+
+
 @Injectable()
 export class ExampleDataService extends TerraBaseService
 {
@@ -18,20 +18,19 @@ export class ExampleDataService extends TerraBaseService
         super(_loadingSpinnerService, _http, '/rest/');
         if(process.env.ENV !== 'production')
         {
-            //tslint:disable-next-line:max-line-length
-            this.bearer = 'YOUR_TOKEN_HERE';
+            // tslint:disable-next-line:max-line-length
+            this.bearer = 'fSEi9jGnOlGUNYspmCaOw6KjAfg-MkpL6MkH91cz9GHeRluLzIP89jGKxabeArvcvBhMJQ557UMge4usumqi68BeiKH-_0odgQXihH18IMQLJR0ZKBC5LFUg0EUCSb5BynW-dQXujBbT2UaVyWw3VFhnzf7orEFYG6i9obQd8XuHy0KtJtISClMZO7-bQVXpSfGQJLdMjvlBvnnBxOqC5HGE-rGCrqWAnuNmK7lZ4EIaD2L8fkXryEHHRk-y1C_o9HFByvVZse4RGMPFciJW2NZ2ATJjfhbJCcQ1liUaLM46pecT66608pUleJ0xQ_33tIo6JA973OAx0drliAZzLVnBeA24aj2EJMQhZ_3kwvXPwbtWY3Euww_FjIYV_6Jsp_dQr8IWeTlyNPCFBRCuCZg8qe1RAw4cipmkKCYpggET5zdXQBI';
             this._basePathUrl = 'http://master.login.plentymarkets.com';
             this.url = this._basePathUrl + this.url;
         }
         this.setHeader();
     }
- 
+
     public getRestCallData(restRoute:string):Observable <Array<any>>
     {
         this.setAuthorization();
         let url:string;
         url = this._basePathUrl + restRoute;
- 
         return this.mapRequest(
             this.http.get(url, {
                 headers: this.headers,
@@ -39,7 +38,7 @@ export class ExampleDataService extends TerraBaseService
             })
         );
     }
- 
+
     private setHeader():void
     {
         if(this.bearer !== null && this.bearer.length > 0)
