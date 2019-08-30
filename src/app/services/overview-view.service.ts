0218d76cae45c11e3e7ce37c72c5e529a0e4fca1
Kevin Fink <kevin.fink@plentymarkets.com>
2019-08-30 11:06:15 +0200

    removed unnecessary header option

--- src/app/services/overview-view.service.ts
+++ src/app/services/overview-view.service.ts
@@ -37,39 +37,33 @@ export class OverviewDataService
     {
         if(!environment.production)
         {
-            this.token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjZmOWFmZDdiZDBkOGMyZDg5MDUzMWZiNjI1MDczYmEzNzgzNWQwYjlmOTZlNTA3NDYyMjI4NDI0MDMyYTE0NjdlYzk3MjFhZjUzOGQ5NmZmIn0.eyJhdWQiOiIxIiwianRpIjoiNmY5YWZkN2JkMGQ4YzJkODkwNTMxZmI2MjUwNzNiYTM3ODM1ZDBiOWY5NmU1MDc0NjIyMjg0MjQwMzJhMTQ2N2VjOTcyMWFmNTM4ZDk2ZmYiLCJpYXQiOjE1NjcxNDU3MDAsIm5iZiI6MTU2NzE0NTcwMCwiZXhwIjoxNTY3MjMyMTAwLCJzdWIiOiIzIiwic2NvcGVzIjpbIioiXX0.Z8wtuVPJc4mhRgqOVNfbDpM8KHafO8OFubvMbllimcWmyRAFXoiNnRpIvgz8_UIHJ-h9Zg0XPf8CeiEy2OopQa0_u1PNRI-r2_xRnN9IllgI95lRDm3bE8tErjzcu_RI3yrg_8re5-slHkEY3GEwIchccjgjf3w3oBhdH_sY2Od0vC7cgQkyaS6MWrRWfkgoaPsv6823--uMsL_EIu7eshABB6jrOkq7UItoO-_xO7As4mF-gwzjf_uhbK_zHdlkQSic-Dg3EknJLujX_uhXTs21oDTKn3zl81cywDMOb1nvWSviNI6zocSNk4ZiaxKkRXfjZ9ZBDUdNfet0L5TiAiKJ2xF4fFpOjHoR_6-SXqmV6KpBgrOA5fe8N_oQ4nK_UThG_i7NGuTLBS_MXkLUcuZm9AaiNGY8LIJMboLN-qbxgpgg6SDUNn4w3f1iIflxC5xUshOiCGoMvlnoEAl1h9ugPcqIpTpD5A6LLCEAe5FHnMT_Ehznip5YK6eyyFeYg1ni4XoPXkVqYjaBEsahzaKXe17AYIpZ1iZ6hzwiG4pOt1bWruPRgu9cyWuwg5b2s_0Singwwg8W7BApfRAj2CYSLF3KZwiV2WRlrlhYuvErKIaFV4YauB1RDSqp-ZzgJyqpXE5zzpKrox7TubbdT31gcno1PhkFRBxxMoaKads';
-            this.url = 'http://master.login.plentymarkets.com/' + this.url;
+            this.token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImE0ODM3OTkzNGE4NzhiYmQyYWQ0NDU1ZDFiNTMxNzUyODA5YTExNTIzNTRiMTI2MTZlNDE1OTFjNGM0YjE2Y2VkNWQ0MjM0M2JmNGU2MjliIn0.eyJhdWQiOiIxIiwianRpIjoiYTQ4Mzc5OTM0YTg3OGJiZDJhZDQ0NTVkMWI1MzE3NTI4MDlhMTE1MjM1NGIxMjYxNmU0MTU5MWM0YzRiMTZjZWQ1ZDQyMzQzYmY0ZTYyOWIiLCJpYXQiOjE1NjcxNDU2NTgsIm5iZiI6MTU2NzE0NTY1OCwiZXhwIjoxNTY3MjMyMDU3LCJzdWIiOiIzIiwic2NvcGVzIjpbIioiXX0.CC0q7GtxnsNJCQs9jvU6VW-9_F-9f_f-5i3eWKRSMvDZW6hN7tPRkfiiWS8TwKZZKPGRxAZk-omN4EUbMY5WmUZlUxgbwbjrgKV1KwRV29MZOz-mv74QU3s0wrFAaGFpRP8FOobU5mO71f7foX3s44B-Wn1PBee7RJ0Xd71tsQvD26dkzaxUhsEarNBvs8JvdvTYjtLiEdyixM3kgBY3gvviamZjQKewmm5gmxX-9QJEwoN6HD8z4VdaRw74AcbcAiCF0kGTsTMI9XolQYsO8LsfSH7y1wuZjJ725fQoRss-HsKygL2hyjAkQKpp87NeZEmVG2F6T0BCHSZCX4DGKiZ_laRZUl6sNJ-5aJB0OL288FMKUa5KIydhWcfGQsHecu0mMswixvbUZIoJOVIns2oCZt1QOlEfGTIvmvLlSiQDytcKRVu62tx32Uu-MOlmKChcV2A6dgIkK85e_nP9wcQpnOdEmLShPVQ9P-xX1tWA3Bo34W4Qofk4-15WVHN5fB7MZeZWwmYBk5q33t_lO7cXgpNLVY7qU8Cbs6Ze9ztCAe34STrgCCkCM3uljeY7vKDSgoJ8h6h15vR8CSgxzZWzf0jM9Hbxqo-s2tIfVcrRQlieobP86W5XLe5L_3RQnpcSIpo0_7m_eWQ3F866rCoupgWvBc8fvV7KQnIraW8';
+            this.url = 'http://master.login.plentymarkets.com' + this.url;
             localStorage.setItem('accessToken', this.token);
         }
     }
 
     public getRestCallData(restRoute:string)
     {
-        return this.http.get(this.url + restRoute, {
-            headers: this.headers
-        });
+        return this.http.get(this.url + '/' + restRoute);
     }
 
 
     public postRestCallData(restRoute:string, data:any):Observable <Object>
     {
-        return this.http.post(this.url + restRoute, data,{
-            headers: this.headers
-        });
+        return this.http.post(this.url + '/' + restRoute, data);
     }
 
     public deleteRestCallData(restRoute:string):Observable <Object>
     {
-        return this.http.delete(this.url + restRoute,{
+        return this.http.delete(this.url + '/' + restRoute,{
             headers: this.headers
         });
     }
 
     public putRestCallData(restRoute:string, data:any):Observable <Object>
     {
-        return this.http.put(this.url + restRoute, data,{
-            headers: this.headers
-        });
+        return this.http.put(this.url + '/' + restRoute, data,);
     }
 
     public isJsonString(str:string):boolean
